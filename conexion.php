49db5eb62b4b4ee9d8f72100564a3d038a765807
Jhonatan Molina <stivendf3@gmail.com>
2024-09-26 17:06:10 -0500

    Se hace la conexion con la base de datos y el login

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // Cambia esto por tu servidor
 $username_db = "root";      // Cambia esto por tu usuario de la base de datos
 $password_db = "";          // Cambia esto por tu contraseña de la base de datos
-$dbname = "widCenter"; // Cambia esto por el nombre de tu base de datos
+$dbname = "login_widcenter"; // Cambia esto por el nombre de tu base de datos
 
 // Crear la conexión
 $conn = new mysqli($servername, $username_db, $password_db, $dbname);
@@ -20,12 +20,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $conn->real_escape_string($_POST['password']);
     
     // Consulta para verificar el nombre de usuario y la contraseña
-    $sql = "SELECT * FROM USUARIOS WHERE correoElectronico = '$username' AND contraseña = '$password'";
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$username' AND contraseña = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // Si el usuario existe, redirigir a una nueva página o mostrar un mensaje de éxito
-        echo "¡Inicio de sesión exitoso!";
+        header("Location: ./pages/afiliate.html");
+        exit();
         // Redirigir al usuario (ejemplo)
         // header("Location: pagina_principal.php");
     } else {
